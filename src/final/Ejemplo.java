c9b8b9386bc3fb67e8a7a98fce262473ab820e14
giovanyhernandez <ghernandezes14dw@ikzubirimanteo.com>
2015-02-04 12:52:21 +0100

    Update Ejemplo.java

--- src/final/Ejemplo.java
+++ src/final/Ejemplo.java
@@ -1,6 +1,6 @@
 public class Ejemplo {  
   public static void main(String[] args) {  
-    final String Cadena = "Hola";  //las constantes son escritas en Mayusculas//
+    final String Cadena = "Holla";  //las constantes son escritas en Mayusculas//
     Cadena = new String("Adios");  
   }  
 }  
